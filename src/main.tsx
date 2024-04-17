991b91ec2fa09f8e81df0c6cc1fa6f01c19e6123
Caio Domingos <caio.domingos@thinkless.com.br>
2024-04-17 10:26:05 -0300

    fix(app.tsx): fix import type

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import App from 'App';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-	QueryClient,
-	QueryClientConfig,
-	QueryClientProvider,
-} from '@tanstack/react-query';
+import type { QueryClientConfig } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { registerSW } from 'virtual:pwa-register';
 import './index.css';
 
